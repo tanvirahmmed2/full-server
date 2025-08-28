d73cc5e64b1d67e542eff8665fd1398411adfd95
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-28 09:50:28 +0600

    update

--- src/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-const connectDB = require("./database/db");
-const {serverPort}= require("./secret")
-
-app.listen(serverPort, async() => {
-  console.log(`🚀 Server running at http://localhost:${serverPort}`);
-  await connectDB()
-});
