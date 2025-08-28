7880937999b99cef02360f80ea17e284417413af
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-28 10:36:20 +0600

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
+require("dotenv").config()
 const express= require('express')
-const PORT= process.env.PORT || 5000
 const app=express()
 const mongoose= require('mongoose')
 const jwt= require('jsonwebtoken')
 const multer= require('multer')
 const path = require('path')
 const cors= require('cors')
+
+const PORT= process.env.PORT || 5000
+const MONGO_URL= process.env.MONGO_URL
+
+
+
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+//mongo connect
+ mongoose.connect(MONGO_URL)
+
+
+//api listen
+
+
 
 
 app.get('/', (req,res)=>{
