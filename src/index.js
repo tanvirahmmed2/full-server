0fb46c1daff01c97ff827e14918978f596c5669b
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-28 23:24:25 +0600

    update

--- src/index.js
+++ src/index.js
@@ -89,6 +89,10 @@ const Product= mongoose.model("Product", {
     old_price:{
         type: Number,
         required: true,
+    },
+    date:{
+        type: Date,
+        default: Date.now,
     }
 
 })
