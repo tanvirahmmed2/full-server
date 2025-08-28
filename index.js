9803640fac3087bb5474de12d73c7f402f6be0cd
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-28 10:18:32 +0600

    update

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express= require('express')
 const PORT= process.env.PORT || 5000
 const app=express()
+const mongoose= require('mongoose')
+const jwt= require('jsonwebtoken')
+const multer= require('multer')
+const path = require('path')
+const cors= require('cors')
+
+
+app.get('/', (req,res)=>{
+    res.send('server running')
+})
 
 
 
-
-
-app.listen(PORT, (req,res)=>{
+app.listen(PORT, ()=>{
     console.log(`Server is running at https://localhost:${PORT}`)
 })
